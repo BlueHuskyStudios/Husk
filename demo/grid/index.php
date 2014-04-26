7a919cb978d2d314e8a2fb82fd61f7241a6074e5
Supuhstar <supuhstar0@gmail.com>
2014-04-26 18:20:02 -0400

    Updated demo
    - Universal HTML classes
    - Updated striped row CSS
    - More sticky testing (should probably move to Husk.css...)
    - Added pretty class to MAIN
    - Added long intro and description to main and each section
    - Changed English text to code text
    - Replaced some generated demos with hand-made ones
    - Commented out old Offset example
    - Made new Offset example
    - Made nesting example
    - Made Mario more like his legit sprite

--- demo/grid/index.php
+++ demo/grid/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE HTML>
-<HTML CLASS="animate-all">
+<?PHP include $_SERVER['DOCUMENT_ROOT'].'/prog/husk/_incl/huskMeta.php'; ?>
+<HTML CLASS="<?PHP echo getHTMLTagClasses(); ?>">
 <HEAD>
 <TITLE>12-Column Grid &ndash; Husk Framework</TITLE>
 <?PHP
@@ -12,6 +13,12 @@ include $_SERVER['DOCUMENT_ROOT'].'/prog/husk/_incl/js.htm';
 MAIN .row.stripes>* {
 	background-color: #27B;
 	color: #FFF;
+}
+MAIN .row.stripes,
+MAIN .row.stripes>* {
+	border-radius: .2em;
+    box-shadow: 0 1px 2px rgba(0,0,0, .5);
+	padding: .2em;
 }
 MAIN .row.stripes>*:nth-child(odd) {
 	background-color: #BBB;
@@ -49,8 +56,8 @@ PRE B.html-class-husk {
 	z-index: 2;
 }
 .sticky-top.stuck {
+	margin: 0 !important;
 	position: fixed;
-	left: 0;
 	top: 0;
 }
 .sticky-top {
@@ -65,7 +72,7 @@ PRE B.html-class-husk {
 aside.big {
 	padding: .5em 0;
 }
-.sticky-top:not(.stuck)+.sticky-clone {
+.sticky-top:not(.stuck)~.sticky-clone {
 	display: none;
 }
 .sticky-clone {
@@ -84,7 +91,7 @@ aside.big {
 <BODY>
 <?PHP $_GET['title'] = '12-Column Grid'; include $_SERVER['DOCUMENT_ROOT'].'/prog/husk/_incl/header.php'; ?>
 
-<MAIN>
+<MAIN CLASS="pretty">
 <ASIDE CLASS="big centered sticky-top" STYLE="width:100%;left:0">You are viewing this in the
 	<B CLASS="show-tiny hide-small">Tiny</B>
 	<B CLASS="hide-tiny show-small">Small</B>
@@ -93,10 +100,27 @@ aside.big {
 	<B CLASS="hide-large show-huge">Huge</B>
 	size
 </ASIDE>
-<P>Below are the elements and their source code, with styling unique to this page. <B>Husk</B> will <EM>never</EM> color your elements.</P>
+<ASIDE>
+	Below are the elements and their source code, with styling unique to this page. <B CLASS="brand">Husk</B> will <EM>never</EM> color your
+	elements.
+</ASIDE>
+
+<HR/>
+
+<P><B CLASS="brand">Husk</B> works on the principle of a <STRONG CLASS="help" DATA-TOOLTIP-TOP="* Husk will come in custom
+	sizes in the future">12-column grid *</STRONG>. This means that cells in a row will be sized in units of <STRONG>twelfths
+	of their parent</STRONG>. This inherently lets you evenly split a page into 12, 6, 4, 3, 2, or 1 column(s), or any
+	combination of mixed sizes.</P>
+<P>To create a <B CLASS="brand">Husk</B> row, you need only give an element the class <CODE>row</CODE> and its children sizing
+	classes (we'll get to that soon). By default, <B CLASS="brand">Husk</B> rows and their cells have <CODE>.5em</CODE> margins, but these can be
+	removed by adding the <CODE>flush</CODE> class to the row.</P>
 <SECTION>
 	<H2><STRONG>Constant-sized</STRONG> cells</H2>
-	<SECTION CLASS="row spaced stripes"><?PHP
+	<P>Let's start simple. You can set any item to a certain width by using <CODE>all-#</CODE>, but replacing
+		the <Q><CODE>#</CODE></Q> with the number of columns you want it to span. <I CLASS="fa fa-info-circle help"
+		DATA-TOOLTIP-TOP="Remember: 1 column represents a twelfth of the width of a row"></I></P>
+	
+	<SECTION CLASS="row stripes"><?PHP
 	for ($i = 12; $i >= 1; $i--)
 	{
 		if ((12 % $i) == 0)
@@ -105,7 +129,7 @@ aside.big {
 			{
 				?>
 
-		<DIV CLASS="all-<?PHP echo $i; ?>">Always <B><?PHP echo $i; ?></B></DIV><?PHP
+		<DIV CLASS="all-<?PHP echo $i; ?>"><CODE>all-<B><?PHP echo $i; ?></B></CODE></DIV><?PHP
 			}
 			?>
 
@@ -117,7 +141,7 @@ aside.big {
 	</SECTION>
 
 	<PRE CLASS="lang-html">
-<B CLASS="html-tag">&lt;SECTION</B> <B CLASS="html-attr">CLASS</B>=<B CLASS="html-attr-value">"<B CLASS="html-class-husk">row spaced</B>"</B>&gt;<?PHP
+<B CLASS="html-tag">&lt;SECTION</B> <B CLASS="html-attr">CLASS</B>=<B CLASS="html-attr-value">"<B CLASS="html-class-husk">row</B>"</B>&gt;<?PHP
 	for ($i = 12; $i >= 6; $i--)
 	{
 		if ((12 % $i) == 0)
@@ -126,7 +150,7 @@ aside.big {
 			{
 				?>
 
-	&lt;DIV CLASS="all-<?PHP echo $i; ?>"&gt;Always &lt;B&gt;<?PHP echo $i; ?>&lt;/B&gt;&lt;/DIV&gt;<?PHP
+	&lt;DIV CLASS="<B CLASS="html-class-husk">all-<?PHP echo $i; ?></B>"&gt;&lt;CODE&gt;all&lt;B&gt;<?PHP echo $i; ?>&lt;/B&gt;&lt;CODE&gt;&lt;/DIV&gt;<?PHP
 			}
 			?>
 
@@ -146,39 +170,36 @@ aside.big {
 
 <SECTION>
 	<H2><STRONG>Responsive-sizing</STRONG> cells</H2>
-	<SECTION CLASS="row stripes spaced"><?PHP
-	
-	for ($i = 0; $i < 12; $i++)
-	{
-		?>
-
-		<DIV CLASS="tiny-12 small-6 medium-3 large-2 huge-1"><B>12</B> tiny, <B>6</B> small, <B>3</B> medium, <B>2</B> large, <B>1</B> huge</DIV><?PHP
-	}
-	?>
-
+	<SECTION CLASS="row stripes">
+		<DIV CLASS="        small-12 medium-6 large-2 huge-1"><CODE>               small-<B>12</B> medium-<B>6</B> large-<B>2</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="tiny-12 small-6  medium-6 large-2 huge-1"><CODE>tiny-<B>12</B> small-<B>6</B>  medium-<B>6</B> large-<B>2</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="tiny-12 small-6  medium-3 large-2 huge-1"><CODE>tiny-<B>12</B> small-<B>6</B>  medium-<B>3</B> large-<B>2</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="        small-12 medium-3 large-2 huge-1"><CODE>               small-<B>12</B> medium-<B>3</B> large-<B>2</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="tiny-12 small-6  medium-3 large-2 huge-1"><CODE>tiny-<B>12</B> small-<B>6</B>  medium-<B>3</B> large-<B>2</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="tiny-12 small-6  medium-3 large-2 huge-1"><CODE>tiny-<B>12</B> small-<B>6</B>  medium-<B>3</B> large-<B>2</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="        small-12 medium-3 large-4 huge-1"><CODE>               small-<B>12</B> medium-<B>3</B> large-<B>4</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="tiny-12 small-6  medium-3 large-4 huge-1"><CODE>tiny-<B>12</B> small-<B>6</B>  medium-<B>3</B> large-<B>4</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="tiny-12 small-6  medium-3 large-4 huge-1"><CODE>tiny-<B>12</B> small-<B>6</B>  medium-<B>3</B> large-<B>4</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="        small-12 medium-3 large-4 huge-1"><CODE>               small-<B>12</B> medium-<B>3</B> large-<B>4</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="tiny-12 small-6  medium-6 large-4 huge-1"><CODE>tiny-<B>12</B> small-<B>6</B>  medium-<B>6</B> large-<B>4</B> huge-<B>1</B></CODE></DIV>
+		<DIV CLASS="tiny-12 small-6  medium-6 large-4 huge-1"><CODE>tiny-<B>12</B> small-<B>6</B>  medium-<B>6</B> large-<B>4</B> huge-<B>1</B></CODE></DIV>
 	</SECTION>
 
 	<PRE>
-&lt;SECTION CLASS="row stripes spaced"><?PHP
-	
-	for ($i = 0; $i < 3; $i++)
-	{
-		?>
-
-	&lt;DIV CLASS="tiny-12 small-6 medium-3 large-2 huge-1"><B>12</B> tiny, <B>6</B> small, <B>3</B> medium, <B>2</B> large, <B>1</B> huge</DIV><?PHP
-	}
-	
-	?>
-
-
-	...
-
+&lt;SECTION CLASS="row stripes">
+		&lt;DIV CLASS="        small-12 medium-6 large-2 huge-1"&gt;&lt;CODE&gt;               small-&lt;B&gt;12&lt;/B&gt; medium-&lt;B&gt;6&lt;/B&gt; large-&lt;B&gt;2&lt;/B&gt; huge-&lt;B&gt;1&lt;/B&gt;&lt;/CODE&gt;&lt;/DIV&gt;
+		&lt;DIV CLASS="tiny-12 small-6  medium-6 large-2 huge-1"&gt;&lt;CODE&gt;tiny-&lt;B&gt;12&lt;/B&gt; small-&lt;B&gt;6&lt;/B&gt;  medium-&lt;B&gt;6&lt;/B&gt; large-&lt;B&gt;2&lt;/B&gt; huge-&lt;B&gt;1&lt;/B&gt;&lt;/CODE&gt;&lt;/DIV&gt;
+		&lt;DIV CLASS="tiny-12 small-6  medium-3 large-2 huge-1"&gt;&lt;CODE&gt;tiny-&lt;B&gt;12&lt;/B&gt; small-&lt;B&gt;6&lt;/B&gt;  medium-&lt;B&gt;3&lt;/B&gt; large-&lt;B&gt;2&lt;/B&gt; huge-&lt;B&gt;1&lt;/B&gt;&lt;/CODE&gt;&lt;/DIV&gt;
+		&lt;DIV CLASS="        small-12 medium-3 large-2 huge-1"&gt;&lt;CODE&gt;               small-&lt;B&gt;12&lt;/B&gt; medium-&lt;B&gt;3&lt;/B&gt; large-&lt;B&gt;2&lt;/B&gt; huge-&lt;B&gt;1&lt;/B&gt;&lt;/CODE&gt;&lt;/DIV&gt;
+		
+		...
+		
 &lt;/SECTION>
 </PRE>
-</SECTION>
+</SECTION><?PHP/*
 
 
-<!--SECTION CLASS="row stripes spaced">
+<SECTION CLASS="row stripes">
 	<?PHP
 	
 	for($i = 1; $i < 12; $i++)
@@ -189,13 +210,13 @@ aside.big {
 	}
 	
 	?>
-</SECTION-->
+</SECTION>
 
 
 
 <SECTION>
 	<H2><STRONG>Offset</STRONG> cells</H2>
-	<SECTION CLASS="row stripes spaced">
+	<SECTION CLASS="row stripes">
 		<DIV CLASS="all-2 offset-all-4">Always <B>2</B>, always offset <B>4</B></DIV> <DIV CLASS="all-1 offset-all-5">Always <B>1</B>, always offset <B>5</B></DIV>
 		<DIV CLASS="all-2 offset-all-3">Always <B>2</B>, always offset <B>3</B></DIV> <DIV CLASS="all-2 offset-all-5">Always <B>2</B>, always offset <B>5</B></DIV>
 		<DIV CLASS="all-2 offset-all-2">Always <B>2</B>, always offset <B>2</B></DIV> <DIV CLASS="all-3 offset-all-5">Always <B>3</B>, always offset <B>5</B></DIV>
@@ -226,6 +247,70 @@ aside.big {
 
 &lt;/SECTION>
 </PRE>
+</SECTION>*/?>
+
+
+
+<SECTION>
+	<H2><STRONG>Offset</STRONG> cells</H2>
+	<SECTION CLASS="row stripes">
+		<DIV CLASS="all-12">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-10">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-4 offset-all-3">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-3">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-2">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-4">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-2">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV> <DIV CLASS="all-2 offset-all-2">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-2">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-2">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV>
+		
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-2">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV> <DIV CLASS="all-2 offset-all-2">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-2">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-1">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-2">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-4">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-2">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-4 offset-all-3">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-3">&zwnj;</DIV>
+		<DIV CLASS="all-1 ">&zwnj;</DIV> <DIV CLASS="all-1 offset-all-10">&zwnj;</DIV>
+		<DIV CLASS="all-12">&zwnj;</DIV>
+	</SECTION>
+
+	<PRE>
+&lt;SECTION CLASS="row stripes"&gt;
+	
+	...
+	
+	&lt;DIV CLASS="all-1 "&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-1"&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-1"&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-2"&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-1"&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-1"&gt;&lt;/DIV&gt;
+	
+	&lt;DIV CLASS="all-1 "&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-1"&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-1"&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-2"&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-1"&gt;&lt;/DIV&gt; &lt;DIV CLASS="all-1 offset-all-1"&gt;&lt;/DIV&gt;
+	
+	...
+	
+&lt;/SECTION&gt;
+</PRE>
+</SECTION>
+
+
+
+<SECTION>
+	<H2><STRONG>Nested</STRONG> rows</H2>
+	<SECTION CLASS="row stripes">
+		<DIV CLASS="all-4 small-12 row stripes">
+			<DIV CLASS="all-4">You can</DIV>
+			<DIV CLASS="all-8">nest any cells</DIV>
+		</DIV>
+		<DIV CLASS="all-8 small-12 row stripes">
+			<DIV CLASS="all-8">within any other cells or rows, without any bulky</DIV>
+			<DIV CLASS="all-4">surrounding elements</DIV>
+		</DIV>
+	</SECTION>
+
+	<PRE>
+&lt;SECTION CLASS="row stripes"&gt;
+	&lt;DIV CLASS="all-4 small-12 row stripes"&gt;
+		&lt;DIV CLASS="all-4"&gt;You can&lt;/DIV&gt;
+		&lt;DIV CLASS="all-8"&gt;nest any cells&lt;/DIV&gt;
+	&lt;/DIV&gt;
+	&lt;DIV CLASS="all-8 small-12 row stripes"&gt;
+		&lt;DIV CLASS="all-8"&gt;within any other cells or rows, without any bulky&lt;/DIV&gt;
+		&lt;DIV CLASS="all-4"&gt;surrounding elements&lt;/DIV&gt;
+	&lt;/DIV&gt;
+&lt;/SECTION&gt;
+</PRE>
 </SECTION>
 
 <SECTION CLASS="row flush centered">
@@ -253,12 +338,15 @@ aside.big {
 			<DIV CLASS="all-8 offset-all-2 mario-tan">Jaw</DIV>
 		</SECTION>
 			
-		<SECTION CLASS="row flush">
-			<DIV CLASS="all-3 offset-all-1 mario-brown">Shirt</DIV><DIV CLASS="all-1 mario-red">Overalls</DIV><DIV CLASS="all-3 mario-brown">Shirt</DIV>
-		</SECTION>
-		<SECTION CLASS="row flush">
-			<DIV CLASS="all-4 mario-brown">Shirt</DIV><DIV CLASS="all-1 mario-red">Overalls</DIV><DIV CLASS="all-1 mario-brown">Shirt</DIV><DIV CLASS="all-1 mario-red">Overalls</DIV><DIV CLASS="all-3 mario-brown">Shirt</DIV>
-		</SECTION>
+		<section class="row flush">
+			<div class="all-2 offset-all-2 mario-brown">Shirt</div><div class="all-1 mario-red">Overalls</div><div class="all-4 mario-brown">Shirt</div>
+		</section>
+		<section class="row flush">
+			<div class="all-3 offset-all-1 mario-brown">Shirt</div><div class="all-1 mario-red">Overalls</div><div class="all-2 mario-brown">Shirt</div><div class="all-1 mario-red">Overalls</div><div class="all-3 mario-brown">Shirt</div>
+		</section>
+		<section class="row flush">
+			<div class="all-4 mario-brown">Shirt</div><div class="all-4 mario-red">Overalls</div><div class="all-4 mario-brown">Shirt</div>
+		</section>
 		<SECTION CLASS="row flush">
 			<DIV CLASS="all-2 mario-tan">Hand</DIV><DIV CLASS="all-1 mario-brown">Shirt</DIV><DIV CLASS="all-1 mario-red">Overalls</DIV><DIV CLASS="all-1 mario-tan">Button</DIV><DIV CLASS="all-2 mario-red">Overalls</DIV><DIV CLASS="all-1 mario-tan">Button</DIV><DIV CLASS="all-1 mario-red">Overalls</DIV><DIV CLASS="all-1 mario-brown">Shirt</DIV><DIV CLASS="all-2 mario-tan">Hand</DIV>
 		</SECTION>
