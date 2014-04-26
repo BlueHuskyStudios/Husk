07bddadbb0cef695d64b6ad70e9fd9406148a26d
Supuhstar <supuhstar0@gmail.com>
2014-04-26 18:52:25 -0400

    Created footer incl to manage all page footers

---
+++ _incl/footer.php
@@ -0,0 +1,48 @@
+<FOOTER CLASS="animate-medium animate-large">
+	<!--FORM CLASS="flex-row flex-horiz-space-between flex-vert-center show-when-jquery" ONCHANGE="fixPage()">
+		This will show when jQuery is used
+	</FORM-->
+	<SECTION CLASS="row flush flex-row flex-horiz-space-between flex-vert-center">
+		<ASIDE CLASS="large-5 medium-4 small-12 text-center"><B CLASS="brand">Husk</B> Framework is copyright <A HREF="/">Blue Husky Programming</a> ©2014 GPLv3
+			<ASIDE CLASS="text-end font-small"><A HREF="./legal" CLASS="read-more">More legal</A></ASIDE>
+		</ASIDE>
+		
+		<SECTION CLASS="centered large-3 medium-5 small-12">
+			<H2 STYLE="margin-top:0" class="font-1x font-normal">Help keep this great project afloat!</H2>
+			<FORM ACTION="https://www.paypal.com/cgi-bin/webscr" METHOD="post" TARGET="_top">
+				<INPUT TYPE="hidden" NAME="cmd" VALUE="_s-xclick">
+				<INPUT TYPE="hidden" NAME="hosted_button_id" VALUE="UZUQAEZALSXAL">
+				<INPUT TYPE="image"  SRC="http://bhstudios.org/_images/adhd/Donate_via_PayPal.png" BORDER="0" NAME="submit" ALT="PayPal - The safer, easier way to pay online!">
+				<IMG ALT="" BORDER="0" SRC="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" WIDTH="1" HEIGHT="1">
+			</FORM>
+		</SECTION>
+		
+		<SECTION CLASS="large-2 medium-3 small-12 flex-row flex-vert-center flex-horiz-center flex-nowrap">
+			<UL CLASS="horiz">
+				<LI DATA-TOOLTIP="Fork on GitHub!" DATA-TOOLTIP-POS="top left">
+					<A HREF="//github.com/BlueHuskyStudios/Husk">
+						<SPAN CLASS="fa-stack fa-lg">
+							<I CLASS="fa fa-github                 fa-stack-2x"></i>
+						</SPAN>
+					</A>
+				</LI>
+				<LI DATA-TOOLTIP="Rave on Twitter!" DATA-TOOLTIP-POS="top left">
+					<A HREF="//twitter.com/BHS_Husk">
+						<SPAN CLASS="fa-stack fa-lg">
+							<I CLASS="fa fa-circle                 fa-stack-2x"></i>
+							<I CLASS="fa fa-twitter fa-inverse     fa-stack-1x"></i>
+						</SPAN>
+					</A>
+				</LI>
+				<LI DATA-TOOLTIP="Connect on Google+!" DATA-TOOLTIP-POS="top left">
+					<A HREF="https://plus.google.com/102126180354643064931" rel="publisher">
+						<SPAN CLASS="fa-stack fa-lg">
+							<I CLASS="fa fa-circle                 fa-stack-2x"></i>
+							<I CLASS="fa fa-google-plus fa-inverse fa-stack-1x"></i>
+						</SPAN>
+					</A>
+				</LI>
+			</UL>
+		</SECTION>
+	</SECTION>
+</FOOTER>
